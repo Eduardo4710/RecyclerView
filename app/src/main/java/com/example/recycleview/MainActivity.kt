a6b7e11d0c40ce179cd593facb7285721ef9ffcf
Eduardo4710 <73405519+Eduardo4710@users.noreply.github.com>
2022-04-21 18:38:20 -0500

    Creamos la funcion setupRecyclerView

--- app/src/main/java/com/example/recycleview/MainActivity.kt
+++ app/src/main/java/com/example/recycleview/MainActivity.kt
@@ -2,10 +2,22 @@ package com.example.recycleview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.recycleview.adapter.ProductsAdapter
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setupRecyclerView()
+    }
+
+
+    private fun setupRecyclerView(){
+        val recyclerView= findViewById<RecyclerView>(R.id.recyclerProduct)
+        recyclerView.layoutManager=LinearLayoutManager(this)
+        recyclerView.adapter=ProductsAdapter(ProductsList.produts)
+
     }
 }
