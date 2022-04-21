0f08cbca7cd2369e4964a502cf73c964f09629ba
Eduardo4710 <73405519+Eduardo4710@users.noreply.github.com>
2022-04-21 18:39:17 -0500

    Inicializamos los atributos de la data class

--- app/src/main/java/com/example/recycleview/Product.kt
+++ app/src/main/java/com/example/recycleview/Product.kt
@@ -1,8 +1,8 @@
 package com.example.recycleview
 
-data class Product(val name:String,
-                   val prece:Int,
-                   val category:String,
-                   val existence:Int,
-                   val photo:String) {
+data class Product(val name:String="",
+                   val prece:Int=0,
+                   val category:String="",
+                   val existence:Int=0,
+                   val photo:String="") {
 }
