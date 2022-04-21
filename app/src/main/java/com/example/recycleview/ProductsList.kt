bad1143e906d972ff69eaf77b11dca707a5471e0
Eduardo4710 <73405519+Eduardo4710@users.noreply.github.com>
2022-04-21 17:42:48 -0500

    Creamos la clase ProductsList y agregamos un listOf de tip Products

---
+++ app/src/main/java/com/example/recycleview/ProductsList.kt
@@ -0,0 +1,15 @@
+package com.example.recycleview
+
+class ProductsList {
+    companion object{
+        val produts = listOf<Product>(
+            Product("Soda",15,"Bebidas",5,"https://i5.walmartimages.com/asr/d6f012c5-f952-43ec-829b-11de16106a61.47195a5a4251fa846e6d99e6560af58c.jpeg"),
+            Product("Soda",15,"Bebidas",5,"https://i5.walmartimages.com/asr/d6f012c5-f952-43ec-829b-11de16106a61.47195a5a4251fa846e6d99e6560af58c.jpeg"),
+            Product("Soda",15,"Bebidas",5,"https://i5.walmartimages.com/asr/d6f012c5-f952-43ec-829b-11de16106a61.47195a5a4251fa846e6d99e6560af58c.jpeg"),
+            Product("Soda",15,"Bebidas",5,"https://i5.walmartimages.com/asr/d6f012c5-f952-43ec-829b-11de16106a61.47195a5a4251fa846e6d99e6560af58c.jpeg")
+
+
+        )
+    }
+
+}
