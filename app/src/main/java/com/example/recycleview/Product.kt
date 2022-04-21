19345a7ed918cf126cba81a79980274a87815c22
Eduardo4710 <73405519+Eduardo4710@users.noreply.github.com>
2022-04-21 17:20:19 -0500

    Creamos la data class Product

---
+++ app/src/main/java/com/example/recycleview/Product.kt
@@ -0,0 +1,8 @@
+package com.example.recycleview
+
+data class Product(val name:String,
+                   val prece:Int,
+                   val category:String,
+                   val existence:Int,
+                   val photo:String) {
+}
