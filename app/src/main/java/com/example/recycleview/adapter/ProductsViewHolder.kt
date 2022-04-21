35d5ce367ca477e427b68b44193305c853c73908
Eduardo4710 <73405519+Eduardo4710@users.noreply.github.com>
2022-04-21 18:37:01 -0500

    configuramos el viewHolder

--- app/src/main/java/com/example/recycleview/adapter/ProductsViewHolder.kt
+++ app/src/main/java/com/example/recycleview/adapter/ProductsViewHolder.kt
@@ -1,4 +1,28 @@
 package com.example.recycleview.adapter
 
-class ProductsViewHolder {
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.recycleview.Product
+import com.example.recycleview.R
+import org.w3c.dom.Text
+
+class ProductsViewHolder(view:View):RecyclerView.ViewHolder(view) {
+
+    val namePdt=view.findViewById<TextView>(R.id.tvName_product)
+    val precePdt=view.findViewById<TextView>(R.id.tvPrece_product)
+    val existPdt=view.findViewById<TextView>(R.id.tvExist_product)
+    val categoryPdt=view.findViewById<TextView>(R.id.tvCategory_product)
+    val photoPdt=view.findViewById<ImageView>(R.id.iv_products)
+
+    fun render(productsModel: Product){
+        namePdt.text=productsModel.name
+        precePdt.text=productsModel.prece.toString()
+        existPdt.text=productsModel.existence.toString()
+        categoryPdt.text=productsModel.category
+
+
+
+    }
 }
