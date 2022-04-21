8f93578c38729775f405ded13c727168c1fed469
Eduardo4710 <73405519+Eduardo4710@users.noreply.github.com>
2022-04-21 18:36:19 -0500

    Agregamos y configuramos los metodos Abstractos de la clases RecyclerView

--- app/src/main/java/com/example/recycleview/adapter/ProductsAdapter.kt
+++ app/src/main/java/com/example/recycleview/adapter/ProductsAdapter.kt
@@ -1,4 +1,24 @@
 package com.example.recycleview.adapter
 
-class ProductsAdapter {
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.recycleview.Product
+import com.example.recycleview.R
+
+class ProductsAdapter(private val nProduts:List<Product>): RecyclerView.Adapter<ProductsViewHolder>() {
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ProductsViewHolder {
+    val layoutInflater=LayoutInflater.from(parent.context).inflate(R.layout.item_products,parent,false)
+      return ProductsViewHolder(layoutInflater)
+    }
+
+    override fun onBindViewHolder(holder: ProductsViewHolder, position: Int) {
+       val item=nProduts[position]
+        holder.render(item)
+    }
+
+    override fun getItemCount(): Int=nProduts.size
+
 }
