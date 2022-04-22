55cd18f58305209d160cff0df181ab2a4dd9a6ac
Eduardo4710 <73405519+Eduardo4710@users.noreply.github.com>
2022-04-22 14:40:52 -0500

    Quitamos el DividerItemDecoration

--- app/src/main/java/com/example/recycleview/MainActivity.kt
+++ app/src/main/java/com/example/recycleview/MainActivity.kt
@@ -4,6 +4,8 @@ import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.recycleview.adapter.ProductsAdapter
@@ -21,14 +23,17 @@ class MainActivity : AppCompatActivity() {
 
 
     private fun setupRecyclerView(){
+    //    val manager =GridLayoutManager(this,2)
+        val manager =LinearLayoutManager(this)
+        //val decoration=DividerItemDecoration(this,manager.orientation)
         val recyclerView= binding.recyclerProduct
-        recyclerView.layoutManager=LinearLayoutManager(this)
+        recyclerView.layoutManager=manager
         recyclerView.adapter=ProductsAdapter(ProductsList.produts) { product ->
             onItemSelected(
                 product
             )
         }
-
+       // binding.recyclerProduct.addItemDecoration(decoration)
     }
 
     fun onItemSelected(product: Product){
